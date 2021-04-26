c9acdcbc7c86918b34a065439a1f2725450e3c0b
Kaku <akash.sharma@rtcamp.com>
2021-04-26 19:04:05 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: Basic Quote Block
+ * Description: A basic quote block.
+ * Author: Akash Sharma
+ * Author URI: http://github.com/00kaku
+
+ * @package 'basic'
+ * Version: 1.0
+ */
+
+/**
+ * Function to enqueue scripts for the block.
+
+ * @return void
+ */
+function enqueue_scripts() {
+	$asset_file = include plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+	wp_register_script(
+		'basic-quote-block',
+		plugins_url( 'build/index.js', __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+	wp_register_style(
+		'basic-quote-block-style-editor',
+		plugins_url( 'src/editor.css', __FILE__ ),
+		array( 'wp-edit-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'src/editor.css' )
+	);
+
+	wp_register_style(
+		'basic-quote-block-style',
+		plugins_url( 'src/style.css', __FILE__ ),
+		array(),
+		filemtime( plugin_dir_path( __FILE__ ) . 'src/style.css' )
+	);
+
+	register_block_type(
+		'basic/quote-block',
+		array(
+			'api_version'   => 2,
+			'editor_script' => 'basic-quote-block',
+			'style'         => 'basic-quote-block-style',
+			'editor_style'  => 'basic-quote-block-style-editor',
+		)
+	);
+}
+
+add_action( 'init', 'enqueue_scripts' );
---
+++ src/index.js
@@ -0,0 +1,98 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, RichText, MediaUpload } from '@wordpress/block-editor';
+import { ColorPicker, Button } from '@wordpress/components';
+import quotes from './assets/quotes.svg';
+registerBlockType( 'basic/quote-block', {
+	apiVersion: 2,
+	title: 'Basic Quote Block',
+	icon: 'lightbulb',
+	category: 'design',
+	attributes:{
+		quote:{
+			type:'string',
+		},
+		author:{
+			 type: 'string',
+		},
+		citation:{
+			 type: 'string',
+		},
+		color:{
+			type: 'string'
+		},
+		imgUrl:{
+			type:'string'
+		}
+	},
+    edit: ( {attributes, setAttributes} ) => {
+		const blockProps = useBlockProps();
+
+		return (
+			<div { ...blockProps }>
+			<h2> BASIC QUOTE BLOCK </h2>
+
+			<h6>Enter the quote</h6>
+			<RichText
+				value= { attributes.quote }
+				onChange={ (quote) => setAttributes( { quote }) }
+				placeholder={('Quote...' ) }/>
+
+			<h6>Enter the citation </h6>
+			<RichText
+				value= { attributes.citation }
+				onChange={ (citation) => setAttributes( { citation }) }
+				placeholder={('Citation...' ) }/>
+
+			<h6>Enter the author </h6>
+			<RichText
+				value= { attributes.author }
+				onChange={ (author) => setAttributes( { author }) }
+				placeholder={('Author...' ) }/>
+
+			<h6>Pick the color theme</h6>
+			 <ColorPicker
+			color={ attributes.color }
+			onChangeComplete={ ( value ) => setAttributes( { color: value.hex } ) }
+			disableAlpha
+			/>
+			<div className="media__manager">
+				{ attributes.imageUrl && (
+				<div>
+				<img src={ attributes.imageUrl }  style={{ 'width':'50px', 'height':'50px' }} />
+				<Button className="button" onClick={()=> setAttributes( {imageUrl:null} ) }>Remove Image</Button>
+				</div> )
+				}
+				<MediaUpload
+					onSelect = { ( img ) =>setAttributes( { imageUrl: img.url } )}
+					allowedTypes={ ['image'] }
+					value = { attributes.imageUrl }
+					render ={ ( { open } ) => <Button className='button' onClick={ () => open()}>Upload author image</Button> }
+				/>
+			</div>
+			</div>
+		);
+	},
+	save: ( {attributes} ) => {
+		const blockProps = useBlockProps.save();
+		return (
+			 <div {...blockProps}>
+
+				<div className="quote__header" style={{'border-top': `2px solid ${attributes.color}`}}>
+					<img src = { quotes } className="quotes__image" />
+					<p className="quote__quote">{attributes.quote}</p>
+				</div>
+				<div className="quote__footer">
+					<div className="quote__footer__text">
+						<h3 style={{'color': attributes.color}}>{attributes.citation}</h3>
+						<h4>{attributes.author}</h4>
+					</div>
+					<div className="image__outer">
+						<div className="image">
+							<img src={ attributes.imageUrl || 'https://via.placeholder.com/50x50'}/>
+						</div>
+					</div>
+				</div>
+			 </div>
+			);
+	},
+});
